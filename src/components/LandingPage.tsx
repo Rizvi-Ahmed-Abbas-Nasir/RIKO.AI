50568d653d83fa6efd0659a765736a378c887263
Rizvi-Ahmed-Abbas-Nasir <abbasrizvi200209@gmail.com>
2026-01-22 00:55:14 +0530

    new codess

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -82,7 +82,7 @@ export default function RikoHome() {
 
   try {
    
-const response = await fetch("http://riko-backend.vercel.app/api/RikoChat", {
+const response = await fetch("https://riko-backend.vercel.app/api/RikoChat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
